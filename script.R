1833a0d9c841e8e65b9580da720b348e630f0e61
nelson-io <nelsonshilman@gmail.com>
2020-06-10 14:49:36 -0300

    tidy empleo_empresas tables

--- script.R
+++ script.R
@@ -3,7 +3,7 @@ library(rio)
 library(googledrive)
 library(janitor)
 
-drive_auth('clopezpinto.gcba@gmail.com', cache = gargle::gargle_oauth_cache())
+drive_auth('nelsonshilman.gcba@gmail.com', cache = gargle::gargle_oauth_cache())
 #drive_find(pattern = 'vbp', n_max = 20)
 
 drive_download(file = as_id('1NFN__acvMtrNUji4H9eZ35rUnx11xECI'), overwrite = TRUE)
@@ -13,7 +13,32 @@ drive_download(file = as_id('1nkE1Mti-O2KiyZ-U4zka_0x3oI7bVnra'), overwrite = TR
 #IMPORTAR DATOS A R
 empleo_empresas_df <- import_list('empleo y empresas.xlsx')
 
-test <- empleo_empresas_df$`empleados caba` %>%
-  gather(key = 'fecha', value = 'empleados', -(1:4)) %>% 
-  clean_names() %>%
-  mutate(fecha = excel_numeric_to_date(as.numeric(fecha)))
+
+
+
+empleo_empresas_df$`empleados caba` <- empleo_empresas_df$`empleados caba` %>%
+  gather(key = 'periodo', value = 'empleados', -(1:3)) %>% 
+  clean_names() %>% 
+  mutate(periodo = excel_numeric_to_date(as.numeric(periodo)))
+
+empleo_empresas_df$`empleadores caba` <- empleo_empresas_df$`empleadores caba` %>% 
+  gather(key = 'periodo', value = 'empleadores', -(1:3)) %>% 
+  clean_names() %>% 
+  mutate(periodo = excel_numeric_to_date(as.numeric(periodo)))
+
+empleo_empresas_df$`empleo nac` <- empleo_empresas_df$`empleo nac` %>% 
+  gather(key = 'year', value = 'empleados', -c(1,2)) %>% 
+  clean_names() %>% 
+  mutate_at(.vars = c(1,4), as.numeric)
+
+empleo_empresas_df$`establecimiento nac` <- empleo_empresas_df$`establecimiento nac` %>% 
+  gather(key = 'year', value = 'establecimientos', -c(1,2)) %>% 
+  clean_names() %>% 
+  mutate_at(.vars = c(1,4), as.numeric)
+
+  
+vbp_vab_pimp <- import_list("vbp, vab y pimp.xlsx")
+
+
+
+
